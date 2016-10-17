684770f3d5b26f702da24f963148293804627d52
Andrei <agmuntianu@yahoo.com>
2016-10-17 17:47:04 +0200

    replaced repeating code with short nibble function

--- cod_sera_test_edit/cod_sera_test_edit/cod_sera_test_edit.ino
+++ cod_sera_test_edit/cod_sera_test_edit/cod_sera_test_edit.ino
@@ -38,6 +38,22 @@ void loop()
 {
 }
 
+bool writeNibble(byte b1, byte b2, byte b3, byte b4)
+{
+	if ((0 <= b1 <= 1) &&
+		(0 <= b1 <= 1) &&
+		(0 <= b1 <= 1) &&
+		(0 <= b1 <= 1))
+	{
+		digitalWrite(IN1, b1);
+		digitalWrite(IN2, b2);
+		digitalWrite(IN3, b3);
+		digitalWrite(IN4, b4);
+		return true;
+	}
+	return false;
+}
+
 void reverse(int i, int j) {
 
 	// set both motors ON
@@ -45,34 +61,22 @@ void reverse(int i, int j) {
 	digitalWrite(ENB, HIGH);
 
 	while (1) {
-		digitalWrite(IN1, 0);
-		digitalWrite(IN2, 1);
-		digitalWrite(IN3, 0);
-		digitalWrite(IN4, 1);
+		writeNibble(0, 1, 0, 1);
 		delay(j);
 		i--;
 		if (i < 1) break;
 
-		digitalWrite(IN1, 0);
-		digitalWrite(IN2, 1);
-		digitalWrite(IN3, 1);
-		digitalWrite(IN4, 0);
+		writeNibble(0, 1, 1, 0);
+		delay(j);
+		i--;
+		if (i < 1) break;
+		
+		writeNibble(1, 0, 1, 0);
 		delay(j);
 		i--;
 		if (i < 1) break;
 
-		digitalWrite(IN1, 1);
-		digitalWrite(IN2, 0);
-		digitalWrite(IN3, 1);
-		digitalWrite(IN4, 0);
-		delay(j);
-		i--;
-		if (i < 1) break;
-
-		digitalWrite(IN1, 1);
-		digitalWrite(IN2, 0);
-		digitalWrite(IN3, 0);
-		digitalWrite(IN4, 1);
+		writeNibble(1, 0, 0, 1);
 		delay(j);
 		i--;
 		if (i < 1) break;
